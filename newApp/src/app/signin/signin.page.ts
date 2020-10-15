25fc4800219f09d8d982b09a5e61d900d4ccd263
LizzyModau26 <69306774+LizzyModau26@users.noreply.github.com>
2020-10-15 09:33:29 +0200

    new ionic app

---
+++ newApp/src/app/signin/signin.page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'signin.component.html',
+  styleUrls: ['signin.component.scss'],
+})
+export class SigninPage {
+
+  constructor() {}
+
+}
